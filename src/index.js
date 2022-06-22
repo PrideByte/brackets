8627f8dede173d3c11e648adbfea44c0bb3b67fb
PrideByte <alpha5.2@mail.ru>
2022-06-22 10:51:47 +0300

    feat: task solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let lexicon = {};
+	let result = new Array();
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    lexicon[bracketsConfig[i][0]] = bracketsConfig[i][1];
+  }
+	
+	for (let i = 0; i < str.length; i++) {
+		if (lexicon[result[result.length - 1]] === str[i]) {
+      result.pop();
+			continue;
+    }
+		if (Object.keys(lexicon).includes(str[i])) {
+      result.push(str[i]);
+			continue;
+    }
+		return false;
+	}
+	
+	return str.length > 0 && result.length === 0;
 }
